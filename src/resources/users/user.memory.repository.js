a06b51b097e03a092ae55b43d65c53c4790fd18e
Alesia Kuptsova <alesya.kuptsova@gmail.com>
2021-05-13 22:57:36 +0200

    feat: start Express REST service task

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,19 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
-};
+const User = require('./user.model');
 
-module.exports = { getAll };
+const users = [];
+const getAll = async () => users;
+const getUserById = async id => users.find(user => user.id === id);
+
+const createUser = async user => {
+    const newUser = new User({
+      id: null,
+      name: user.name,
+      login: user.login,
+      password: user.password
+    });
+    users.push(newUser);
+    return newUser;
+  }
+
+
+module.exports = { getAll, getUserById, createUser };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -8,4 +8,24 @@ router.route('/').get(async (req, res) => {
   res.json(users.map(User.toResponse));
 });
 
+router.route('/:id').get(
+  async (req, res) => {
+    const user = await usersService.getUserById(req.params.id);
+    if (user) {
+      res.status(200).json(User.toResponse(user));
+    } else {
+      const error = new Error();
+      error.status = 404;
+      throw error;
+    }
+  }
+);
+
+router.route('/').post(
+ async (req, res) => {
+    const user = await usersService.createUser(req.body);
+    res.status(200).json(User.toResponse(user));
+  }
+);
+
 module.exports = router;
