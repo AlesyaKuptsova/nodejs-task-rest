35752aa943f2b65e5cecccb6fd00eb1217630d33
Alesia Kuptsova <alesya.kuptsova@gmail.com>
2021-05-27 15:53:30 +0200

    docs: add jsdoc for boards

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -3,10 +3,24 @@ const taskService = require('../tasks/task.service');
 
 const boards = [];
 
+/**
+ * Get all boards.
+ * @returns {Promise<Board[]>} array containing all boards
+ */
 const getAll = async () => boards;
 
+/**
+ * Get the board by ID.
+ * @param {string} id board ID
+ * @returns {Promise<Board>} board by ID
+ */
 const getBoardById = async (id) => boards.find((board) => board.id === id);
 
+/**
+ * Create board.
+ * @param {object} data board data
+ * @return {Promise<Board>} new board
+ */
 const createBoard = async (data) => {
   const newBoard = new Board({
     title: data.title,
@@ -16,6 +30,12 @@ const createBoard = async (data) => {
   return newBoard;
 };
 
+/**
+ * Update board.
+ * @param {string} id board ID
+ * @param {object} data board data
+ * @return {Promise<Board>} changed board, null if doesn't exist
+ */
 const updateBoard = async (id, data) => {
   const index = boards.findIndex((board) => board.id === id);
   if (index < 0) {
@@ -26,6 +46,11 @@ const updateBoard = async (id, data) => {
   return boards[index];
 };
 
+/**
+ * Delete board.
+ * @param {string} id board ID
+ * @return {Promise<boolean>} whether the board was deleted
+ */
 const deleteBoard = async (id) => {
   const index = boards.findIndex((board) => board.id === id);
   if (index < 0) {
--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,14 +1,39 @@
 const boardsRepo = require('./board.memory.repository');
 const { deleteTasksByBoardId } = require('../tasks/task.service');
 
+/**
+ * Get all boards.
+ * @returns {Promise<Board[]>} array containing all boards
+ */
 const getAll = () => boardsRepo.getAll();
 
+/**
+ * Create board.
+ * @param {object} data board data
+ * @returns {Promise<Board>} new board
+ */
 const createBoard = (data) => boardsRepo.createBoard(data);
 
+/**
+ * Get the board by ID.
+ * @param {string} id board ID
+ * @returns {Promise<Board>} board by ID
+ */
 const getBoardById = (id) => boardsRepo.getBoardById(id);
 
+/**
+ * Update board.
+ * @param {string} id board ID
+ * @param {object} data board data
+ * @returns {Promise<Board>} changed board, null if doesn't exist
+ */
 const updateBoard = (id, data) => boardsRepo.updateBoard(id, data);
 
+/**
+ * Delete board.
+ * @param {string} id board ID
+ * @return {Promise<boolean>} whether the board was deleted
+ */
 const deleteBoard = async (id) => {
   await deleteTasksByBoardId(id);
   return boardsRepo.deleteBoard(id);
