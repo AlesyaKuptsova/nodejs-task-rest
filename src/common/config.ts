d5e66f35fb37a5b7d6ed82bc157ae6f6dd192186
Alesia Kuptsova <alesya.kuptsova@gmail.com>
2021-06-12 23:19:58 +0200

    feat: add db docker

--- src/common/config.ts
+++ src/common/config.ts
@@ -3,11 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  PORT: process.env['PORT'],
+  PORT: process.env['NODE_PORT'],
+  HOST: process.env['NODE_HOST'],
   NODE_ENV: process.env['NODE_ENV'],
   MONGO_CONNECTION_STRING: process.env['MONGO_CONNECTION_STRING'],
   JWT_SECRET_KEY: process.env['JWT_SECRET_KEY'],
-  AUTH_MODE: process.env['AUTH_MODE'] === 'true',
+  AUTH_MODE: process.env['NODE_AUTH_MODE'] === 'true',
 };
 
 export { config };
