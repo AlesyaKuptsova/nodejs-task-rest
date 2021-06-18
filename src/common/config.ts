663c0d86070d98e1abe121d8d79118f36d6e61a3
Alesia Kuptsova <alesya.kuptsova@gmail.com>
2021-06-18 21:39:56 +0200

    feat: start postgresql and typeorm task

--- src/common/config.ts
+++ src/common/config.ts
@@ -6,7 +6,6 @@ const config = {
   PORT: process.env['NODE_PORT'],
   HOST: process.env['NODE_HOST'],
   NODE_ENV: process.env['NODE_ENV'],
-  MONGO_CONNECTION_STRING: process.env['MONGO_CONNECTION_STRING'],
   JWT_SECRET_KEY: process.env['JWT_SECRET_KEY'],
   AUTH_MODE: process.env['NODE_AUTH_MODE'] === 'true',
 };
